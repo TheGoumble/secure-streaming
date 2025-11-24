361f02aaf05929760fad9e01048879744fb1b73a
Javier Andres Vargas <80772713+TheGoumble@users.noreply.github.com>
2025-11-23 23:52:37 -0500

    updated readme / test config
    Dont freak out i am setting the port default to 8080 since its the standard, I only had it different due to going issues with the ports remaining active. As long as the port numbers match app handshake is possible

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 // The base URL for your Spring Boot server
 export const BACKEND_HOST = "localhost";
-export const BACKEND_PORT = "8086";
+export const BACKEND_PORT = "8080";
 
 // HTTP URL for the Viewer's MJPEG stream (e.g., http://localhost:8080)
 export const HTTP_BASE_URL = `http://${BACKEND_HOST}:${BACKEND_PORT}`;
